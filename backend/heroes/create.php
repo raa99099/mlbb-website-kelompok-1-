040883420154318579585fd8142a0a369efde51d
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:35:39 +0700

    Update create.php

--- backend/heroes/create.php
+++ backend/heroes/create.php
@@ -1,1 +1,12 @@
+<?php
+include "../config/db.php";
 
+$nama = $_POST['nama'];
+$role = $_POST['role'];
+$damage = $_POST['damage'];
+
+mysqli_query($conn,
+"INSERT INTO heroes(nama,role,damage)
+VALUES('$nama','$role','$damage')");
+
+header("Location: ../../frontend/heroes.php");
