3910ce990925d6516ed69d36f6be5a48a2516666
ourelliey <relaurelrel@gmail.com>
2026-05-30 21:03:56 +0700

    Update create.php

--- backend/items/create.php
+++ backend/items/create.php
@@ -1,1 +1,13 @@
+<?php
+include "../config/db.php";
 
+$nama  = $_POST['nama'];
+$harga = $_POST['harga'];
+
+mysqli_query($conn,
+"INSERT INTO items (nama, harga)
+VALUES ('$nama', '$harga')");
+
+header("Location: ../../frontend/items.php");
+exit;
+?>
