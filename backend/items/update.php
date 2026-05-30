fd556221f79fff6b57ef604a68220d55ee67a10b
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:44:21 +0700

    Implement item update functionality in update.php

--- backend/items/update.php
+++ backend/items/update.php
@@ -1,1 +1,16 @@
+<?php
+include "../config/db.php";
 
+$id    = $_POST['id'];
+$nama  = $_POST['nama'];
+$harga = $_POST['harga'];
+
+mysqli_query($conn,
+"UPDATE items
+SET nama='$nama',
+harga='$harga'
+WHERE id='$id'");
+
+header("Location: ../../frontend/items.php");
+exit;
+?>
