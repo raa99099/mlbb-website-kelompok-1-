bf84f7aba720daa61bb982afc32685aab6549331
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:37:39 +0700

    Update function.php

--- backend/function.php
+++ backend/function.php
@@ -1,1 +1,19 @@
+<?php
 
+function totalHeroes($conn){
+    return mysqli_num_rows(
+        mysqli_query($conn,"SELECT * FROM heroes")
+    );
+}
+
+function totalItems($conn){
+    return mysqli_num_rows(
+        mysqli_query($conn,"SELECT * FROM items")
+    );
+}
+
+function totalTeams($conn){
+    return mysqli_num_rows(
+        mysqli_query($conn,"SELECT * FROM teams")
+    );
+}
