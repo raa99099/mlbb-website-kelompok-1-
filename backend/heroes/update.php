b47321c8f734e0252f7029a960494717a9864aa2
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:36:26 +0700

    Implement hero update functionality
    Update hero details in the database using POST data.

--- backend/heroes/update.php
+++ backend/heroes/update.php
@@ -1,1 +1,16 @@
+<?php
+include "../config/db.php";
 
+$id = $_POST['id'];
+$nama = $_POST['nama'];
+$role = $_POST['role'];
+$damage = $_POST['damage'];
+
+mysqli_query($conn,
+"UPDATE heroes
+SET nama='$nama',
+role='$role',
+damage='$damage'
+WHERE id='$id'");
+
+header("Location: ../../frontend/heroes.php");
