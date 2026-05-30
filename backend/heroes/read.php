b1c0c0cb08a8d46d4374a2965697ec85c6d6e662
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:48:58 +0700

    Update read.php

--- backend/heroes/read.php
+++ backend/heroes/read.php
@@ -1,13 +1,29 @@
 <?php
-include "../config/db.php";
+include "../backend/config/db.php";
 
 $query = mysqli_query($conn, "SELECT * FROM heroes");
-
-$data = [];
-
-while($row = mysqli_fetch_assoc($query)){
-    $data[] = $row;
-}
-
-return $data;
 ?>
+
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Role</th>
+        <th>Damage</th>
+        <th>Aksi</th>
+    </tr>
+
+    <?php while($hero = mysqli_fetch_assoc($query)) : ?>
+    <tr>
+        <td><?= $hero['id']; ?></td>
+        <td><?= $hero['nama']; ?></td>
+        <td><?= $hero['role']; ?></td>
+        <td><?= $hero['damage']; ?></td>
+        <td>
+            <a href="edit_hero.php?id=<?= $hero['id']; ?>">Edit</a>
+            <a href="../backend/heroes/delete.php?id=<?= $hero['id']; ?>">Hapus</a>
+        </td>
+    </tr>
+    <?php endwhile; ?>
+
+</table>
