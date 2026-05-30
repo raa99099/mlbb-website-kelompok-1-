1c9d0a4599e723907d952e9cee23410219e22462
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:46:40 +0700

    Fetch all teams from the database in read.php

--- backend/teams/read.php
+++ backend/teams/read.php
@@ -1,1 +1,13 @@
+<?php
+include "../config/db.php";
 
+$query = mysqli_query($conn, "SELECT * FROM teams");
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($query)){
+    $data[] = $row;
+}
+
+return $data;
+?>
