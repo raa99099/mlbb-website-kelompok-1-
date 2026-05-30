83a9b1d071e0f4c9bba587109f0e953954c45da5
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:46:13 +0700

    Update create.php

--- backend/teams/create.php
+++ backend/teams/create.php
@@ -1,1 +1,13 @@
+<?php
+include "../config/db.php";
 
+$nama_team = $_POST['nama_team'];
+$asal      = $_POST['asal'];
+
+mysqli_query($conn,
+"INSERT INTO teams (nama_team, asal)
+VALUES ('$nama_team', '$asal')");
+
+header("Location: ../../frontend/teams.php");
+exit;
+?>
