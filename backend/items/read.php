03b03e7bc423e88f288b42706a0e8f3a35641dfb
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:43:58 +0700

    Update read.php

--- backend/items/read.php
+++ backend/items/read.php
@@ -1,1 +1,13 @@
+<?php
+include "../config/db.php";
 
+$query = mysqli_query($conn, "SELECT * FROM items");
+
+$data = [];
+
+while($row = mysqli_fetch_assoc($query)){
+    $data[] = $row;
+}
+
+return $data;
+?>
