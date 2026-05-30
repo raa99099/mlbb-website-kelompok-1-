f45766c733e9c70da73aed89166771c922b4fe12
ourelliey <relaurelrel@gmail.com>
2026-05-30 20:47:00 +0700

    Update update.php

--- backend/teams/update.php
+++ backend/teams/update.php
@@ -1,1 +1,16 @@
+<?php
+include "../config/db.php";
 
+$id        = $_POST['id'];
+$nama_team = $_POST['nama_team'];
+$asal      = $_POST['asal'];
+
+mysqli_query($conn,
+"UPDATE teams
+SET nama_team='$nama_team',
+asal='$asal'
+WHERE id='$id'");
+
+header("Location: ../../frontend/teams.php");
+exit;
+?>
